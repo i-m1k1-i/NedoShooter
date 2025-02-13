a24f6ac5943747582b1420673cce66542baba785
m1k1 <belmirbek7@gmail.com>
2025-02-13 19:08:09 +0600

    Semi-automatic guns recoil added

--- Assets/Scripts/Weapons/Base/SemiAutomatic.cs
+++ Assets/Scripts/Weapons/Base/SemiAutomatic.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.LivingEntities.Player;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -12,38 +13,69 @@ namespace Assets.Scripts.Weapons
         [SerializeField] protected Transform _camera;
         [SerializeField] protected int _damage;
 
+        [Header("Recoil")]
+        [SerializeField] protected float _defaultRecoilPower = 1f;
+        [SerializeField] protected float _recoilPowerIncreasingRate = 0.1f;
+
+        protected float _recoilDecreasingRate => _recoilPower * 0.025f;
+        private RotationController _rotationController;
+        private Animator _animator;
+        private bool _safetyEnabled = false;
+        private float _recoilPower;
+        private float _addedRecoil;
+
         public Vector3 InHandPosition { get; protected set; }
         public int CurrentAmmo { get; protected set; }
         public int MagazineCapacity { get; protected set; }
-
-        private bool _safetyEnabled = false;
-
 
         // Should be used in the last string of a child class's Awake
         protected void Init()
         {
             _camera = transform.parent;
+            Transform player = _camera.parent;
+
+            _rotationController = player.GetComponent<RotationController>();
+            _animator = GetComponent<Animator>();
+
             CurrentAmmo = MagazineCapacity;
             InHandPosition = new Vector3(0f, 0f, 0f);
         }
 
+        private void Update()
+        {
+            if (_addedRecoil > 0)
+            {
+                _rotationController.AddRotateX(-_recoilDecreasingRate);
+                _addedRecoil = Mathf.Clamp(_addedRecoil - _recoilDecreasingRate, 0, 1000);
+            }
+            else
+            {
+                _recoilPower = _defaultRecoilPower;
+            }
+        }
+
         public virtual void Attack()
         {
+            Debug.Log("Semi attack");
             if (CurrentAmmo == 0 ||
                 _safetyEnabled)
             {
+                Debug.Log("Semi attack cancel");
                 return;
             }
 
             RaycastHit[] hits = Physics.RaycastAll(_camera.position, _camera.forward, 500);
+            Debug.Log("Semi attack raycast");
             foreach (RaycastHit hit in hits)
             {
                 if (hit.transform.TryGetComponent<Health>(out Health health))
                 {
+                    Debug.Log("Semi attack damage");
                     health.TakeDamage(_damage);
                     print(hit.transform.name);
                 }
             }
+            ApplyRecoil();
             CurrentAmmo--;
             Attacked?.Invoke();
             AmmoAmountChanged?.Invoke();
@@ -51,8 +83,10 @@ namespace Assets.Scripts.Weapons
 
         public virtual int Reload(int extraAmmo)
         {
+            Debug.Log("Semi Reload");
             if (extraAmmo == 0)
             {
+                Debug.Log("Semi Reload Extra 0");
                 return 0;
             }
 
@@ -74,6 +108,13 @@ namespace Assets.Scripts.Weapons
             return usedAmmo;
         }
 
+        private void ApplyRecoil()
+        {
+            _rotationController.AddRotateX(_recoilPower);
+            _addedRecoil += _recoilPower;
+            _recoilPower += _recoilPowerIncreasingRate;
+        }
+
         public void SafetyOff()
         {
             _safetyEnabled = false;
@@ -83,6 +124,12 @@ namespace Assets.Scripts.Weapons
         public void SafetyOn()
         {
             _safetyEnabled = true;
+        }
+
+        private void OnEnable()
+        {
+            _animator.SetTrigger("Take");
+            Debug.Log("In Hand Pos: " + InHandPosition);
         }
 
         private void OnDisable()
